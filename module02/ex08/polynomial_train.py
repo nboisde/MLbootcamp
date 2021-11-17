31e7214eaa7cf3f1fbaf640ce0c3cf5d612962b5
Nicolas BOISDE <nboisde@e1r6p11.42.fr>
2021-11-17 12:39:49 +0100

    polynomial train done

--- module02/ex08/polynomial_train.py
+++ module02/ex08/polynomial_train.py
@@ -2,10 +2,13 @@ import pandas as pd
 import numpy as np
 from mylinearregression import MyLinearRegression
 from polynomial_model import add_polynomial_features
+import matplotlib.pyplot as plt
 
 data = pd.read_csv('../are_blue_pills_magics.csv')
 X = np.array(data[['Micrograms']])
 Y = np.array(data[['Score']])
+
+print("data loaded")
 
 #MODEL1
 X1 = add_polynomial_features(X, 1)
@@ -60,3 +63,43 @@ model6.fit_(X6, Y)
 YH6 = model6.predict_(X6)
 M6 = model6.mse_(Y, YH6)
 print(f"model6 MSE : {M6}")
+
+#MSE = [M1, M2, M3, M4, M5, M6]
+#MSE = [310.3630318037389,190.2975161969467,160.7012916341579,31.79389326073696,28.496664713160218,4.0682650199467005]
+#
+#x = [1, 2, 3, 4, 5, 6]
+#plt.hist(x, weights=MSE)
+#plt.show()
+
+plt.scatter(X, Y)
+CONT_X = np.arange(1.1, 6.5, 0.01).reshape(-1, 1)
+#print(CONT_X)
+CONT_YH1 = model1.predict_(CONT_X)
+plt.plot(CONT_X, CONT_YH1, color='lightblue')
+
+CONT_X2 = add_polynomial_features(CONT_X, 2)
+#print(CONT_X2)
+CONT_YH2 = model2.predict_(CONT_X2)
+plt.plot(CONT_X, CONT_YH2, color='lightgreen')
+
+CONT_X3 = add_polynomial_features(CONT_X, 3)
+#print(CONT_X2)
+CONT_YH3 = model3.predict_(CONT_X3)
+plt.plot(CONT_X, CONT_YH3, color='purple')
+
+CONT_X4 = add_polynomial_features(CONT_X, 4)
+##print(CONT_X2)
+CONT_YH4 = model4.predict_(CONT_X4)
+plt.plot(CONT_X, CONT_YH4, color='purple')
+
+CONT_X5 = add_polynomial_features(CONT_X, 5)
+##print(CONT_X2)
+CONT_YH5 = model5.predict_(CONT_X5)
+plt.plot(CONT_X, CONT_YH5, color='#D820C2')
+
+CONT_X6 = add_polynomial_features(CONT_X, 6)
+##print(CONT_X2)
+CONT_YH6 = model6.predict_(CONT_X6)
+plt.plot(CONT_X, CONT_YH6, color='#F0D705')
+
+plt.show()

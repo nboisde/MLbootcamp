c9cbc8e2bd2202c4ba9435569821ff4bcf91d1d0
Nicolas BOISDE <nboisde@e1r6p11.42.fr>
2021-11-18 16:14:31 +0100

    space avocado jupyter

--- module02/ex10/space_avocado.py
+++ module02/ex10/space_avocado.py
@@ -8,151 +8,32 @@ from data_splitter import data_splitter
 from polynomial_model import add_polynomial_features
 
 
-#univariate model funciton
-def univariate_model(data, feature1, target, proportion=0.80, alph=1e-7, max_it=100000, normal=0, model_name="univariate_polynomial_model", norm=0):
-	X = np.array(data[[feature1]])
-	Y = np.array(data[[target]])
-	if norm == 0:
-		Xm=X
-		Xm=Y
-	elif norm == 1:
-		Xm = minmax(X)
-		Ym = minmax(Y)
-	else:
-		Xm = zscore(X)
-		Ym = zscore(Y)
-	clean_set = data_splitter(Xm, Ym, proportion)
-	Y_TRAIN = clean_set[2]
-	X_TRAIN = clean_set[0]
-	Y_TEST = clean_set[3]
-	X_TEST = clean_set[1]
-	myLR_f = MyLR(theta = [[0.0], [0.0]], alpha=alph, max_iter=max_it)
-	if normal == 1:
-		myLR_f.theta = myLR_f.normal_equation(X_TRAIN, Y_TRAIN)
-	else:
-		myLR_f.fit_(X_TRAIN, Y_TRAIN)
-	print(f"{model_name} feat: {feature1}")
-	print(f"theta : {myLR_f.theta}")
-	print(myLR_f.mse_(myLR_f.predict_(X_TEST), Y_TEST))
-
-#univariate poly model
-def univariate_polynomial_model(data, feature1, t, proportion=0.80, alph=1e-8, max_it=100000, polyfeat=2, th=[[0.0], [0.0]], normal=0, model_name="univariate_polynomial_model", norm=0):
-	X = np.array(data[[feature1]])
-	X = add_polynomial_features(X, polyfeat)
-	Y = np.array(data[[t]])
-	if norm == 0:
-		Xm=X
-		Xm=Y
-	elif norm == 1:
-		Xm = minmax(X)
-		Ym = minmax(Y)
-	else:
-		Xm = zscore(X)
-		Ym = zscore(Y)
-	clean_set = data_splitter(Xm, Ym, proportion)
-	Y_TRAIN = clean_set[2]
-	X_TRAIN = clean_set[0]
-	Y_TEST = clean_set[3]
-	X_TEST = clean_set[1]
-	myLR_f = MyLR(th, alpha=alph, max_iter=max_it)
-	if normal == 1:
-		myLR_f.theta = myLR_f.normal_equation(X_TRAIN, Y_TRAIN)
-	else:
-		myLR_f.fit_(X_TRAIN, Y_TRAIN)
-	print(f"{model_name} polynomial order: {polyfeat}, feat {feature1}")
-	print(f"theta : {myLR_f.theta}")
-	print(myLR_f.mse_(myLR_f.predict_(X_TEST), Y_TEST))
-
-#multivariate model
-def multivariate_model(data, features, t, proportion=0.80, alph=1e-8, max_it=100000, th=[[0.0], [0.0]], normal=0, model_name="multivariate_model", norm=0):
-	if not isinstance(features, list):
-		raise TypeError("Must have a feature")
-	for val in features:
-		if not isinstance(val, str):
-			raise TypeError("features must be strings")
-	X = np.array(data[features])
-	Y = np.array(data[[t]])
-	if norm == 0:
-		Xm=X
-		Xm=Y
-	elif norm == 1:
-		Xm = minmax(X)
-		Ym = minmax(Y)
-	else:
-		Xm = zscore(X)
-		Ym = zscore(Y)
-	clean_set = data_splitter(Xm, Ym, proportion)
-	Y_TRAIN = clean_set[2]
-	X_TRAIN = clean_set[0]
-	Y_TEST = clean_set[3]
-	X_TEST = clean_set[1]
-	myLR_f = MyLR(th, alpha=alph, max_iter=max_it)
-	if normal == 1:
-		myLR_f.theta = myLR_f.normal_equation(X_TRAIN, Y_TRAIN)
-	else:
-		myLR_f.fit_(X_TRAIN, Y_TRAIN)
-	print(f"{model_name} features:{features}")
-	print(f"theta : {myLR_f.theta}")
-	print(myLR_f.mse_(myLR_f.predict_(X_TEST), Y_TEST))
-
-
-if __name__ == "__main__":
-	d = pd.read_csv("../space_avocado.csv")
-	univariate_model(d, 'weight', 'target', normal=1, norm=1)
-	univariate_model(d, 'prod_distance', 'target', normal=1, norm=1)
-	univariate_model(d, 'time_delivery', 'target', normal=1, norm=1)
-	univariate_polynomial_model(d, 'weight', 'target', polyfeat=2, alph=1e-7, normal=1, norm=1)
-	univariate_polynomial_model(d, 'time_delivery', 'target', polyfeat=2, alph=1e-7, normal=1, norm=1)
-	univariate_polynomial_model(d, 'prod_distance', 'target', polyfeat=2, alph=1e-7, normal=1, norm=1)
-	univariate_polynomial_model(d, 'weight', 'target', polyfeat=3, alph=1e-11, th=[[0],[0],[0],[0]], normal=1, norm=1)
-	univariate_polynomial_model(d, 'time_delivery', 'target', polyfeat=3, alph=1e-11, th=[[0],[0],[0],[0]], normal=1, norm=1)
-	univariate_polynomial_model(d, 'prod_distance', 'target', polyfeat=3, alph=1e-11, th=[[0],[0],[0],[0]], normal=1, norm=1)
-	univariate_polynomial_model(d, 'weight', 'target', polyfeat=4, alph=1e-11, th=[[0],[0],[0],[0],[0]], normal=1, norm=1)
-	univariate_polynomial_model(d, 'time_delivery', 'target', polyfeat=4, alph=1e-11, th=[[0],[0],[0],[0],[0]], normal=1, norm=1)
-	univariate_polynomial_model(d, 'prod_distance', 'target', polyfeat=4, alph=1e-11, th=[[0],[0],[0],[0],[0]], normal=1, norm=1)
-	multivariate_model(d, ['prod_distance', 'time_delivery'], 'target', proportion=0.80, alph=1e-8, max_it=100000, th=[[0.0], [0.0], [0.0]], normal=1, norm=1)
-	multivariate_model(d, ['prod_distance', 'weight'], 'target', proportion=0.80, alph=1e-8, max_it=100000, th=[[0.0], [0.0], [0.0]], normal=1, norm=1)
-	multivariate_model(d, ['weight', 'time_delivery'], 'target', proportion=0.80, alph=1e-8, max_it=100000, th=[[0.0], [0.0], [0.0]], normal=1, norm=1)
-	multivariate_model(d, ['prod_distance', 'weight', 'time_delivery'], 'target', proportion=0.80, alph=1e-8, max_it=100000, th=[[0.0], [0.0], [0.0], [0.0]], normal=1, norm=1)
-	#multivariate_model(d, ['prod_distance', 'time_delivery', 'weight'], 'target', proportion=0.80, alph=1e-11, max_it=10000000, th=[[0.0], [0.0], [0.0], [0.0]], normal=0)
-
-	# last try
-	X = np.array(d[['prod_distance', 'weight', 'time_delivery']])
-	Y = np.array(d[['target']])
-	Xm = minmax(X)
-	Ym = minmax(Y)
-	x0 = add_polynomial_features(Xm[:,0], 2)
-	x1 = add_polynomial_features(Xm[:,1], 2)
-	x2 = add_polynomial_features(Xm[:,2], 2)
-	Xl = np.c_[x0, x1, x2]
-	clean_set = data_splitter(Xl, Ym, 0.80)
-	Y_TRAIN = clean_set[2]
-	X_TRAIN = clean_set[0]
-	Y_TEST = clean_set[3]
-	X_TEST = clean_set[1]
-	myLR_f = MyLR([[0],[0],[0],[0],[0],[0],[0]])
-	myLR_f.theta = myLR_f.normal_equation(X_TRAIN, Y_TRAIN)
-	print(f"theta : {myLR_f.theta}")
-	print(myLR_f.mse_(myLR_f.predict_(X_TEST), Y_TEST))
-
-	X = np.array(d[['prod_distance', 'weight', 'time_delivery']])
-	Y = np.array(d[['target']])
-	Xm = minmax(X)
-	Ym = minmax(Y)
-	x0 = add_polynomial_features(Xm[:,0], 2)
-	x1 = add_polynomial_features(Xm[:,1], 3)
-	x2 = add_polynomial_features(Xm[:,2], 1)
-	Xl = np.c_[x0, x1, x2]
-	clean_set = data_splitter(Xl, Ym, 0.80)
-	Y_TRAIN = clean_set[2]
-	X_TRAIN = clean_set[0]
-	Y_TEST = clean_set[3]
-	X_TEST = clean_set[1]
-	myLR_f = MyLR([[0],[0],[0],[0],[0],[0],[0]])
-	myLR_f.theta = myLR_f.normal_equation(X_TRAIN, Y_TRAIN)
-	print(f"theta : {myLR_f.theta}")
-	print(myLR_f.mse_(myLR_f.predict_(X_TEST), Y_TEST))
-
-	myLR_f.plot_uni(X_TEST[:,0], Y_TEST, myLR_f.predict_(X_TEST), "x1: weight", "y: target(minmax normalization)", "target", "predicted target")
-	myLR_f.plot_uni(X_TEST[:,1], Y_TEST, myLR_f.predict_(X_TEST), "x1: prod_distance", "y: target(minmax normalization)", "target", "predicted target", coly='green', colyh='lightgreen')
-	myLR_f.plot_uni(X_TEST[:,2], Y_TEST, myLR_f.predict_(X_TEST), "x1: time_delivery", "y: target(minmax normalization)", "target", "predicted target", coly='purple', colyh='#b19cd9')
+d = pd.read_csv("../space_avocado.csv")
+d.head()
+X = np.array(d[['weight', 'prod_distance', 'time_delivery']])
+Y = np.array(d[['target']])
+Xm = minmax(X)
+Ym = minmax(Y)
+x0 = add_polynomial_features(Xm[:,0], 2)
+x1 = add_polynomial_features(Xm[:,1], 3)
+x2 = add_polynomial_features(Xm[:,2], 1)
+Xl = np.c_[x0, x1, x2]
+print(Xl)
+clean_set = data_splitter(Xl, Ym, 0.80)
+Y_TRAIN = clean_set[2]
+X_TRAIN = clean_set[0]
+Y_TEST = clean_set[3]
+X_TEST = clean_set[1]
+#plt.scatter(X_TRAIN[:,1], Y_TRAIN)
+#plt.show()
+#plt.scatter(X_TRAIN[:,0], Y_TRAIN)
+#plt.show()
+#plt.scatter(X_TRAIN[:,2], Y_TRAIN)
+#plt.show()
+myLR_f = MyLR([[0],[0],[0],[0],[0],[0],[0]])
+myLR_f.theta = myLR_f.normal_equation(X_TRAIN, Y_TRAIN)
+print(f"theta : {myLR_f.theta}")
+print(myLR_f.mse_(myLR_f.predict_(X_TEST), Y_TEST))
+#myLR_f.plot_uni(X_TEST[:,0], Y_TEST, myLR_f.predict_(X_TEST), "x1: weight", "y: target(minmax normalization)", "target", "predicted target")
+#myLR_f.plot_uni(X_TEST[:,2], Y_TEST, myLR_f.predict_(X_TEST), "x1: prod_distance", "y: target(minmax normalization)", "target", "predicted target", coly='green', colyh='lightgreen')
+#myLR_f.plot_uni(X_TEST[:,5], Y_TEST, myLR_f.predict_(X_TEST), "x1: time_delivery", "y: target(minmax normalization)", "target", "predicted target", coly='purple', colyh='#b19cd9')
--- module03/ex03/vec_log_loss.py
+++ module03/ex03/vec_log_loss.py
@@ -1,6 +1,5 @@
 import numpy as np
 import math
-import copy
 
 def sigmoid_(x):
 	if not isinstance(x, np.ndarray):
@@ -32,8 +31,7 @@ def logistic_predict_(x, theta):
 	tmp = x.dot(theta)
 	return sigmoid_(tmp)
 
-# cross entropy
-def log_loss_(y, y_hat, eps=1e-15):
+def vec_log_loss_(y, y_hat, eps=1e-15):
 	if not isinstance(y, np.ndarray) or not isinstance(y_hat, np.ndarray):
 		raise TypeError("y and y_hat should be numpy arrays")
 	if y_hat.ndim == 1:
@@ -45,39 +43,33 @@ def log_loss_(y, y_hat, eps=1e-15):
 	for yv, yhv in zip(y, y_hat):
 		tmpy = []
 		tmpyh = []
-		if yv == 0:
-			tmpy.append(eps)
-		else:
-			tmpy.append(yv[0])
-		if yhv == 0:
-			tmpyh.append(eps)
-		else:
-			tmpyh.append(yhv[0])
+		tmpy.append(eps) if yv == 0 else tmpy.append(yv[0])
+		tmpyh.append(eps) if yhv == 0 else tmpyh.append(yhv[0])
 		y2.append(tmpy)
 		y_hat2.append(tmpyh)
-	sum = 0
-	print(y2)
-	print(y_hat2)
-	for yv, yhv in zip(y2, y_hat2):
-		tmp = (yv[0] * math.log(yhv[0])) + ((1 - yv[0]) * math.log(1 - yhv[0]))
-		sum += tmp
-	return ((-1) / y.size) * sum
-	
+	y2 = np.array(y2)
+	y_hat2 = np.array(y_hat2)
+	ones = np.ones((y.size, 1))
+	tmp = np.transpose(y2).dot(np.log(y_hat2))[0][0]
+	tmp2 = np.transpose((ones - y2)).dot(np.log(ones - y_hat2))[0][0]
+	return (-1/(y.size)) * (tmp + tmp2)
+
 
 y1 = np.array([1])
 x1 = np.array([1])
 theta1 = np.array([[2], [0.5]])
 y_hat1 = logistic_predict_(x1, theta1)
-print(log_loss_(y1, y_hat1))
+print(vec_log_loss_(y1, y_hat1))
 
 y2 = np.array([[1], [0], [1], [0], [1]])
 x2 = np.array([[4], [7.16], [3.2], [9.37], [0.56]])
 theta2 = np.array([[2], [0.5]])
 y_hat2 = logistic_predict_(x2, theta2)
-print(log_loss_(y2, y_hat2))
+print(vec_log_loss_(y2, y_hat2))
 
 y3 = np.array([[0], [1], [1]])
 x3 = np.array([[0, 2, 3, 4], [2, 4, 5, 5], [1, 3, 2, 7]])
 theta3 = np.array([[-2.4], [-1.5], [0.3], [-1.4], [0.7]])
 y_hat3 = logistic_predict_(x3, theta3)
-print(log_loss_(y3, y_hat3))
+print(vec_log_loss_(y3, y_hat3))
+	

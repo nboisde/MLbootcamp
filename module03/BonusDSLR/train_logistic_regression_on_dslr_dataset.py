90767513b74c0b9e5fb2fdd046171d2e0d14ab69
nboisde <nboisde.d3v@gmail.com>
2021-11-24 13:41:40 +0100

    test with scikit learn

--- module03/BonusDSLR/train_logistic_regression_on_dslr_dataset.py
+++ module03/BonusDSLR/train_logistic_regression_on_dslr_dataset.py
@@ -8,6 +8,7 @@ from data_splitter_one_for_all import data_splitter_one_for_all_v2
 from my_logistic_regression import MyLogisticRegression as MyLR
 from minmax import minmax
 from data_splitter_one_for_all import data_splitter_one_for_all
+from sklearn.svm import SVC
 
 data_training = pd.read_csv("./datasets/dataset_train.csv")
 #hand = pd.get_dummies(data_training['Best Hand'], prefix='hand')
@@ -158,101 +159,152 @@ DF = pd.DataFrame(ALL_PREDS, columns=['Index', 'Gryffindor Probability', 'Huffle
 print(DF)
 DF.to_csv(path_or_buf='predict.csv', sep=',',index=False)
 
-train_set = pd.concat([clean_data[['Index','Divination','Muggle Studies','Care of Magical Creatures','Charms','Flying','Transfiguration']], target, clean_data['Hogwarts House']], axis=1)
-#print(train_set.head())
-train_set.dropna(subset = ['Index','Divination','Muggle Studies','Care of Magical Creatures','Charms','Flying','Transfiguration'], inplace=True)
-print(train_set)
-#g = sns.pairplot(data=clean_data[['Divination','Muggle Studies','Transfiguration','Care of Magical Creatures','Charms','Flying','Hogwarts House']], hue='Hogwarts House')
-#g.fig.set_figheight(7)
-#g.fig.set_figwidth(10)
-#plt.show()
-
-C = np.array(train_set)
-#print(C)
-tpl = data_splitter_one_for_all(C, xfeat=6)
-
-#X_TRAIN = tpl[2]
-#X_TEST = tpl[3]
-X_TRAIN = np.c_[minmax(tpl[2][:,0]),minmax(tpl[2][:,1]),minmax(tpl[2][:,2]),minmax(tpl[2][:,3]),minmax(tpl[2][:,4]),minmax(tpl[2][:,5])]
-X_TEST = np.c_[minmax(tpl[3][:,0]),minmax(tpl[3][:,1]),minmax(tpl[3][:,2]),minmax(tpl[3][:,3]),minmax(tpl[3][:,4]),minmax(tpl[3][:,5])]
-INDEX_TRAIN = tpl[0]
-INDEX_TEST = tpl[1]
-print(tpl[0])
-print(tpl[2])
-print(tpl[4])
-print(np.c_[tpl[0], tpl[2], tpl[4], tpl[6]])
-
-
-#print(np.c_[tpl[0], tpl[2], tpl[4], tpl[6]])
-#print(tpl[4])
-
-# One for all training
-Y_GRIF_TR = tpl[4][:, 0].reshape(-1, 1)
-Y_HUF_TR = tpl[4][:, 1].reshape(-1, 1)
-Y_RAVEN_TR = tpl[4][:, 2].reshape(-1, 1)
-Y_SLYT_TR = tpl[4][:, 3].reshape(-1, 1)
-
-Y_TEST_GRIF = tpl[5][:, 0].reshape(-1, 1)
-Y_TEST_HUF = tpl[5][:, 1].reshape(-1, 1)
-Y_TEST_RAVEN = tpl[5][:, 2].reshape(-1, 1)
-Y_TEST_SLYT = tpl[5][:, 3].reshape(-1, 1)
-
-
-MGRIF = model_training(X_TRAIN, Y_GRIF_TR, X_TEST, Y_TEST_GRIF,th=[[0], [0], [0], [0], [0], [0], [0]],name='Gryffindor', al=0.2, mi=100)
-MHUF = model_training(X_TRAIN, Y_HUF_TR, X_TEST, Y_TEST_HUF,th=[[0], [0], [0], [0], [0], [0], [0]],name='Hufflepuf', al=0.2, mi=100)
-MSLYT = model_training(X_TRAIN, Y_SLYT_TR, X_TEST, Y_TEST_SLYT,th=[[0], [0], [0], [0], [0], [0], [0]],name='Slytherin', al=0.2, mi=100)
-MRAVEN = model_training(X_TRAIN, Y_RAVEN_TR, X_TEST, Y_TEST_RAVEN,th=[[0], [0], [0], [0], [0], [0], [0]],name='Ravenclaw', al=0.2, mi=100)
-
-ALL_PREDS = np.c_[tpl[1], MGRIF[1], MHUF[1], MRAVEN[1], MSLYT[1], tpl[7]]
-print(ALL_PREDS)
-
-DF = pd.DataFrame(ALL_PREDS, columns=['Index', 'Gryffindor Probability', 'Hufflepuff Probability', 'Ravenclaw Probability', 'Slytherin Probability', 'Real House'])
-print(DF)
-
-train_set = pd.concat([clean_data[['Index','Divination','Muggle Studies','Care of Magical Creatures','Potions','Charms','Flying','Transfiguration']], target, clean_data['Hogwarts House']], axis=1)
-#print(train_set.head())
-train_set.dropna(subset = ['Index','Divination','Muggle Studies','Care of Magical Creatures','Potions','Charms','Flying','Transfiguration'], inplace=True)
-print(train_set)
-
-C = np.array(train_set)
-#print(C)
-tpl = data_splitter_one_for_all(C, xfeat=7)
-
-#X_TRAIN = tpl[2]
-#X_TEST = tpl[3]
-X_TRAIN = np.c_[minmax(tpl[2][:,0]),minmax(tpl[2][:,1]),minmax(tpl[2][:,2]),minmax(tpl[2][:,3]),minmax(tpl[2][:,4]),minmax(tpl[2][:,5]),minmax(tpl[2][:,6])]
-X_TEST = np.c_[minmax(tpl[3][:,0]),minmax(tpl[3][:,1]),minmax(tpl[3][:,2]),minmax(tpl[3][:,3]),minmax(tpl[3][:,4]),minmax(tpl[3][:,5]),minmax(tpl[3][:,6])]
-INDEX_TRAIN = tpl[0]
-INDEX_TEST = tpl[1]
+#train_set = pd.concat([clean_data[['Index','Divination','Muggle Studies','Care of Magical Creatures','Charms','Flying','Transfiguration']], target, clean_data['Hogwarts House']], axis=1)
+##print(train_set.head())
+#train_set.dropna(subset = ['Index','Divination','Muggle Studies','Care of Magical Creatures','Charms','Flying','Transfiguration'], inplace=True)
+#print(train_set)
+##g = sns.pairplot(data=clean_data[['Divination','Muggle Studies','Transfiguration','Care of Magical Creatures','Charms','Flying','Hogwarts House']], hue='Hogwarts House')
+##g.fig.set_figheight(7)
+##g.fig.set_figwidth(10)
+##plt.show()
+#
+#C = np.array(train_set)
+##print(C)
+#tpl = data_splitter_one_for_all(C, xfeat=6)
+#
+##X_TRAIN = tpl[2]
+##X_TEST = tpl[3]
+#X_TRAIN = np.c_[minmax(tpl[2][:,0]),minmax(tpl[2][:,1]),minmax(tpl[2][:,2]),minmax(tpl[2][:,3]),minmax(tpl[2][:,4]),minmax(tpl[2][:,5])]
+#X_TEST = np.c_[minmax(tpl[3][:,0]),minmax(tpl[3][:,1]),minmax(tpl[3][:,2]),minmax(tpl[3][:,3]),minmax(tpl[3][:,4]),minmax(tpl[3][:,5])]
+#INDEX_TRAIN = tpl[0]
+#INDEX_TEST = tpl[1]
 #print(tpl[0])
 #print(tpl[2])
 #print(tpl[4])
-print(np.c_[tpl[0], tpl[2], tpl[4], tpl[6]])
-
-
 #print(np.c_[tpl[0], tpl[2], tpl[4], tpl[6]])
-#print(tpl[4])
+#
+#
+##print(np.c_[tpl[0], tpl[2], tpl[4], tpl[6]])
+##print(tpl[4])
+#
+## One for all training
+#Y_GRIF_TR = tpl[4][:, 0].reshape(-1, 1)
+#Y_HUF_TR = tpl[4][:, 1].reshape(-1, 1)
+#Y_RAVEN_TR = tpl[4][:, 2].reshape(-1, 1)
+#Y_SLYT_TR = tpl[4][:, 3].reshape(-1, 1)
+#
+#Y_TEST_GRIF = tpl[5][:, 0].reshape(-1, 1)
+#Y_TEST_HUF = tpl[5][:, 1].reshape(-1, 1)
+#Y_TEST_RAVEN = tpl[5][:, 2].reshape(-1, 1)
+#Y_TEST_SLYT = tpl[5][:, 3].reshape(-1, 1)
+#
+#
+#MGRIF = model_training(X_TRAIN, Y_GRIF_TR, X_TEST, Y_TEST_GRIF,th=[[0], [0], [0], [0], [0], [0], [0]],name='Gryffindor', al=0.2, mi=100)
+#MHUF = model_training(X_TRAIN, Y_HUF_TR, X_TEST, Y_TEST_HUF,th=[[0], [0], [0], [0], [0], [0], [0]],name='Hufflepuf', al=0.2, mi=100)
+#MSLYT = model_training(X_TRAIN, Y_SLYT_TR, X_TEST, Y_TEST_SLYT,th=[[0], [0], [0], [0], [0], [0], [0]],name='Slytherin', al=0.2, mi=100)
+#MRAVEN = model_training(X_TRAIN, Y_RAVEN_TR, X_TEST, Y_TEST_RAVEN,th=[[0], [0], [0], [0], [0], [0], [0]],name='Ravenclaw', al=0.2, mi=100)
+#
+#ALL_PREDS = np.c_[tpl[1], MGRIF[1], MHUF[1], MRAVEN[1], MSLYT[1], tpl[7]]
+#print(ALL_PREDS)
+#
+#DF = pd.DataFrame(ALL_PREDS, columns=['Index', 'Gryffindor Probability', 'Hufflepuff Probability', 'Ravenclaw Probability', 'Slytherin Probability', 'Real House'])
+#print(DF)
+#
+#train_set = pd.concat([clean_data[['Index','Divination','Muggle Studies','Care of Magical Creatures','Potions','Charms','Flying','Transfiguration']], target, clean_data['Hogwarts House']], axis=1)
+##print(train_set.head())
+#train_set.dropna(subset = ['Index','Divination','Muggle Studies','Care of Magical Creatures','Potions','Charms','Flying','Transfiguration'], inplace=True)
+#print(train_set)
+#
+#C = np.array(train_set)
+##print(C)
+#tpl = data_splitter_one_for_all(C, xfeat=7)
+#
+##X_TRAIN = tpl[2]
+##X_TEST = tpl[3]
+#X_TRAIN = np.c_[minmax(tpl[2][:,0]),minmax(tpl[2][:,1]),minmax(tpl[2][:,2]),minmax(tpl[2][:,3]),minmax(tpl[2][:,4]),minmax(tpl[2][:,5]),minmax(tpl[2][:,6])]
+#X_TEST = np.c_[minmax(tpl[3][:,0]),minmax(tpl[3][:,1]),minmax(tpl[3][:,2]),minmax(tpl[3][:,3]),minmax(tpl[3][:,4]),minmax(tpl[3][:,5]),minmax(tpl[3][:,6])]
+#INDEX_TRAIN = tpl[0]
+#INDEX_TEST = tpl[1]
+##print(tpl[0])
+##print(tpl[2])
+##print(tpl[4])
+#print(np.c_[tpl[0], tpl[2], tpl[4], tpl[6]])
+#
+#
+##print(np.c_[tpl[0], tpl[2], tpl[4], tpl[6]])
+##print(tpl[4])
+#
+## One for all training
+#Y_GRIF_TR = tpl[4][:, 0].reshape(-1, 1)
+#Y_HUF_TR = tpl[4][:, 1].reshape(-1, 1)
+#Y_RAVEN_TR = tpl[4][:, 2].reshape(-1, 1)
+#Y_SLYT_TR = tpl[4][:, 3].reshape(-1, 1)
+#
+#Y_TEST_GRIF = tpl[5][:, 0].reshape(-1, 1)
+#Y_TEST_HUF = tpl[5][:, 1].reshape(-1, 1)
+#Y_TEST_RAVEN = tpl[5][:, 2].reshape(-1, 1)
+#Y_TEST_SLYT = tpl[5][:, 3].reshape(-1, 1)
+#
+#
+#MGRIF = model_training(X_TRAIN, Y_GRIF_TR, X_TEST, Y_TEST_GRIF,th=[[0], [0], [0], [0], [0], [0], [0], [0]],name='Gryffindor', al=0.2, mi=100)
+#MHUF = model_training(X_TRAIN, Y_HUF_TR, X_TEST, Y_TEST_HUF,th=[[0], [0], [0], [0], [0], [0], [0], [0]],name='Hufflepuf', al=0.2, mi=100)
+#MSLYT = model_training(X_TRAIN, Y_SLYT_TR, X_TEST, Y_TEST_SLYT,th=[[0], [0], [0], [0], [0], [0], [0], [0]],name='Slytherin', al=0.2, mi=100)
+#MRAVEN = model_training(X_TRAIN, Y_RAVEN_TR, X_TEST, Y_TEST_RAVEN,th=[[0], [0], [0], [0], [0], [0], [0], [0]],name='Ravenclaw', al=0.2, mi=100)
+#
+#ALL_PREDS = np.c_[tpl[1], MGRIF[1], MHUF[1], MRAVEN[1], MSLYT[1], tpl[7]]
+#print(ALL_PREDS)
+#
+#DF = pd.DataFrame(ALL_PREDS, columns=['Index', 'Gryffindor Probability', 'Hufflepuff Probability', 'Ravenclaw Probability', 'Slytherin Probability', 'Real House'])
+#print(DF)
+##DF.to_csv(path_or_buf='predict.csv', sep=',',index=False)
 
-# One for all training
-Y_GRIF_TR = tpl[4][:, 0].reshape(-1, 1)
-Y_HUF_TR = tpl[4][:, 1].reshape(-1, 1)
-Y_RAVEN_TR = tpl[4][:, 2].reshape(-1, 1)
-Y_SLYT_TR = tpl[4][:, 3].reshape(-1, 1)
+svmgr = SVC()
+Y_LINE = Y_GRIF_TR.astype('int').reshape(-1, 1)
+svmgr.fit(X_TRAIN, Y_LINE)
+YPRED_GRIF = svmgr.predict(X_TEST)
 
-Y_TEST_GRIF = tpl[5][:, 0].reshape(-1, 1)
-Y_TEST_HUF = tpl[5][:, 1].reshape(-1, 1)
-Y_TEST_RAVEN = tpl[5][:, 2].reshape(-1, 1)
-Y_TEST_SLYT = tpl[5][:, 3].reshape(-1, 1)
+svmgr = SVC()
+Y_LINE = Y_HUF_TR.astype('int').reshape(-1, 1)
+svmgr.fit(X_TRAIN, Y_LINE)
+YPRED_HUF = svmgr.predict(X_TEST)
 
+svmgr = SVC()
+Y_LINE = Y_RAVEN_TR.astype('int').reshape(-1, 1)
+svmgr.fit(X_TRAIN, Y_LINE)
+YPRED_RAVEN = svmgr.predict(X_TEST)
 
-MGRIF = model_training(X_TRAIN, Y_GRIF_TR, X_TEST, Y_TEST_GRIF,th=[[0], [0], [0], [0], [0], [0], [0], [0]],name='Gryffindor', al=0.2, mi=100)
-MHUF = model_training(X_TRAIN, Y_HUF_TR, X_TEST, Y_TEST_HUF,th=[[0], [0], [0], [0], [0], [0], [0], [0]],name='Hufflepuf', al=0.2, mi=100)
-MSLYT = model_training(X_TRAIN, Y_SLYT_TR, X_TEST, Y_TEST_SLYT,th=[[0], [0], [0], [0], [0], [0], [0], [0]],name='Slytherin', al=0.2, mi=100)
-MRAVEN = model_training(X_TRAIN, Y_RAVEN_TR, X_TEST, Y_TEST_RAVEN,th=[[0], [0], [0], [0], [0], [0], [0], [0]],name='Ravenclaw', al=0.2, mi=100)
+svmgr = SVC()
+Y_LINE = Y_SLYT_TR.astype('int').reshape(-1, 1)
+svmgr.fit(X_TRAIN, Y_LINE)
+YPRED_SLYT = svmgr.predict(X_TEST)
 
-ALL_PREDS = np.c_[tpl[1], MGRIF[1], MHUF[1], MRAVEN[1], MSLYT[1], tpl[7]]
-print(ALL_PREDS)
+ALL_PREDS = np.c_[tpl[1], YPRED_GRIF.reshape(-1, 1), YPRED_HUF.reshape(-1, 1), YPRED_RAVEN.reshape(-1, 1), YPRED_SLYT.reshape(-1, 1), tpl[7]]
+DFSVM = pd.DataFrame(ALL_PREDS, columns=['Index', 'Gryffindor Probability', 'Hufflepuff Probability', 'Ravenclaw Probability', 'Slytherin Probability', 'Real House'])
+print(DFSVM)
+DFSVM.to_csv(path_or_buf='predict_scikit_svm.csv', sep=',',index=False)
 
-DF = pd.DataFrame(ALL_PREDS, columns=['Index', 'Gryffindor Probability', 'Hufflepuff Probability', 'Ravenclaw Probability', 'Slytherin Probability', 'Real House'])
-print(DF)
-#DF.to_csv(path_or_buf='predict.csv', sep=',',index=False)
+from sklearn.ensemble import RandomForestClassifier
+rfg = RandomForestClassifier(max_depth=2, random_state=0)
+Y_LINE = Y_GRIF_TR.astype('int').reshape(-1, 1)
+rfg.fit(X_TRAIN, Y_LINE)
+YPRED_GRIF = rfg.predict(X_TEST)
+
+rfg = RandomForestClassifier(max_depth=2, random_state=0)
+Y_LINE = Y_HUF_TR.astype('int').reshape(-1, 1)
+rfg.fit(X_TRAIN, Y_LINE)
+YPRED_HUF = rfg.predict(X_TEST)
+
+rfg = RandomForestClassifier(max_depth=2, random_state=0)
+Y_LINE = Y_RAVEN_TR.astype('int').reshape(-1, 1)
+rfg.fit(X_TRAIN, Y_LINE)
+YPRED_RAVEN = rfg.predict(X_TEST)
+
+rfg = RandomForestClassifier(max_depth=2, random_state=0)
+Y_LINE = Y_SLYT_TR.astype('int').reshape(-1, 1)
+rfg.fit(X_TRAIN, Y_LINE)
+YPRED_SLYT = rfg.predict(X_TEST)
+
+ALL_PREDS = np.c_[tpl[1], YPRED_GRIF.reshape(-1, 1), YPRED_HUF.reshape(-1, 1), YPRED_RAVEN.reshape(-1, 1), YPRED_SLYT.reshape(-1, 1), tpl[7]]
+DFRF = pd.DataFrame(ALL_PREDS, columns=['Index', 'Gryffindor Probability', 'Hufflepuff Probability', 'Ravenclaw Probability', 'Slytherin Probability', 'Real House'])
+print(DFRF)
+DFRF.to_csv(path_or_buf='predict_scikit_random_forest.csv', sep=',',index=False)
